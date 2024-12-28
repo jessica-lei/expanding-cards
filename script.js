ab86dd22fa9c90ee3a1e8d9e5a2acb381d7453d2
jessica-lei <jessicalei2022@u.northwestern.edu>
2024-12-28 17:41:44 -0500

    reformatted files

--- script.js
+++ script.js
@@ -5,24 +5,26 @@ cards.forEach((card) => {
 window.addEventListener("resize", resize);
 document.addEventListener("keydown", arrowControl);
 
-
 function arrowControl(e) {
-  let ind = Array.from(cards).findIndex(card => card.classList.contains('expanded'));
+  let ind = Array.from(cards).findIndex((card) =>
+    card.classList.contains("expanded")
+  );
 
   if (e.key === "ArrowLeft" && ind > 0) {
     ind -= 1;
-  }
-  else if (e.key === "ArrowRight" && ind < 4) {
+  } else if (e.key === "ArrowRight" && ind < 4) {
     ind += 1;
   }
-
   collapseAll();
-
   cards[ind].classList.add("expanded");
 }
+
 function resize() {
   let currExpanded = document.querySelector(".expanded").id;
-  if (window.innerWidth <= 900 && (currExpanded === "card4" || currExpanded === "card5")) {
+  if (
+    window.innerWidth <= 900 &&
+    (currExpanded === "card4" || currExpanded === "card5")
+  ) {
     collapseAll();
     document.querySelector("#card1").classList.add("expanded");
   }
